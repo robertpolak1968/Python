bb34ec59c8f50a25b84b1f9fc9d34ad0fdbad79b
robertpolak1968@gmail.com <robertpolak1968@gmail.com>
2019-09-18 09:22:58 +0100

    progress

--- test.py
+++ test.py
@@ -6,3 +6,17 @@ This is a temporary script file.
 """
 
 print('eee')
+print ('Ted')
+print ("Robert") # This line prints the surname
+
+age =105
+name = "Ted"
+
+print("Name is ", name, " age is ", age)
+
+print('Welcome to', end=" ")
+print('same line')
+
+print(type(age))
+print(type(name))
+
